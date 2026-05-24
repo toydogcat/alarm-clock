72b63404c106e6d82507d62a5b6025f1ad2b461a
toydogcat <toydogcat@gmail.com>
2026-05-25 01:14:37 +0800

    fix: update asset paths in index.html and configure base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/alarm-clock/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
